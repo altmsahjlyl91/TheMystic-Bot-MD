a032fb37e4745df6cd461d167cb161ef48fd60cb
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-09 01:56:19 +0300

    Update dl-تخيل.js

--- plugins/dl-تخيل.js
+++ plugins/dl-تخيل.js
@@ -2,25 +2,21 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
   if (!text) throw 'What do you want to create?';
-  
-  if (typeof m.react === 'function') {
-    m.react('🕒'); // استخدم رمز الانتظار أو رسالة انتظار
-  } else {
-    console.error("m.react is not a function");
-  }
-  
+
+  // إرسال رسالة انتظار
+  await conn.sendMessage(m.chat, { text: '⏳ Generating image...' }, { quoted: m });
+
   let msg = encodeURIComponent(text);
   let res = await fetch(`https://aemt.me/bingimg?text=${msg}`);
   let data = await res.json();
   console.log(data);
   let buffer = data.result;
-  conn.sendFile(m.chat, buffer, 'image.png', `${text}`, m);
   
-  if (typeof m.react === 'function') {
-    m.react('✅'); // استخدم رمز الانتهاء أو رسالة انتهاء
-  } else {
-    console.error("m.react is not a function");
-  }
+  // إرسال الصورة
+  await conn.sendFile(m.chat, buffer, 'image.png', `${text}`, m);
+
+  // إرسال رسالة إتمام العملية
+  await conn.sendMessage(m.chat, { text: '✅ Image generated successfully!' }, { quoted: m });
 };
 
 handler.help = ['bingimg <query>'];
