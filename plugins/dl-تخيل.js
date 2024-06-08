55f628e4460542f7be1639dc0241690c5dda4964
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-09 01:53:23 +0300

    Update dl-تخيل.js

--- plugins/dl-تخيل.js
+++ plugins/dl-تخيل.js
@@ -1,19 +1,30 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
-  if (!text) throw 'What do you want to create?'
-  m.react(rwait)
-  let msg = encodeURIComponent(text)
-  let res = await fetch(`https://aemt.me/bingimg?text=${msg}`)
-  let data = await res.json()
-  console.log(data)
-  let buffer = data.result
-  conn.sendFile(m.chat, buffer, 'image.png', `${text}`, m)
-  m.react(done)
-}
+  if (!text) throw 'What do you want to create?';
+  
+  if (typeof m.react === 'function') {
+    m.react('🕒'); // استخدم رمز الانتظار أو رسالة انتظار
+  } else {
+    console.error("m.react is not a function");
+  }
+  
+  let msg = encodeURIComponent(text);
+  let res = await fetch(`https://aemt.me/bingimg?text=${msg}`);
+  let data = await res.json();
+  console.log(data);
+  let buffer = data.result;
+  conn.sendFile(m.chat, buffer, 'image.png', `${text}`, m);
+  
+  if (typeof m.react === 'function') {
+    m.react('✅'); // استخدم رمز الانتهاء أو رسالة انتهاء
+  } else {
+    console.error("m.react is not a function");
+  }
+};
 
-handler.help = ['bingimg <query>']
-handler.tags = ['AI']
-handler.command = /^bingimg|تخيل$/i
+handler.help = ['bingimg <query>'];
+handler.tags = ['AI'];
+handler.command = /^bingimg|تخيل$/i;
 
-export default handler
+export default handler;
